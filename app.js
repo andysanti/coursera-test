d7af405d763e87015e7e632e721d64ddc355bf31
Andy Santi Almeida <andysanti23@gmail.com>
2017-08-29 13:42:25 -0500

    inyecion de dependencia

---
+++ app.js
@@ -0,0 +1,20 @@
+(function () {
+'use strict'
+angular.module('DIApp',[])
+.controller('DIController',DIController);
+
+function DIController ($scope,$filter){
+    $scope.name="Andy";
+    $scope.upper= function(){
+        var upCase=$filter('uppercase');
+        $scope.name=upCase($scope.name);
+    };
+    
+}
+function AnnonateMe(name,job,blah){
+    return "Blah!";
+}
+
+    console.log(AnnonateMe.toString());
+
+})();
