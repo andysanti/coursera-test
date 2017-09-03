0b6cfa4d5e85334a1b32fb774a8186299ff4a6d8
Andy Santi Almeida <andysanti23@gmail.com>
2017-09-03 10:54:44 -0500

    data binding

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 (function () {
 'use strict'
 angular.module('MsgApp',[])
-.controller('MsgController',MsgController);
-
-MsgController.$inject= ['$scope'];
-    function MsgController($scope){
+.controller('MsgController',MsgController)
+.filter('loves',LovesFilter)
+.filter('truth',TruthFilter);
+    
+    
+MsgController.$inject= ['$scope','lovesFilter'];
+    function MsgController($scope,lovesFilter){
         $scope.name="Yaakov";
         $scope.stateOfBeing="hungry";
+        $scope.cookiesCost=.45;
         $scope.sayMessage=function(){
-       
-        return "Yaakov likes to eat healthy snacks at night!";
+            var msg="Yaakov likes to eat healthy snacks at night!";
+            
+        return msg;
+        };
+         $scope.sayLovesMessage=function(){
+            var msg="Yaakov likes to eat healthy snacks at night!";
+            var msg=lovesFilter(msg);
+        return msg;
         };
         $scope.feedYaakov=function(){
             $scope.stateOfBeing="fed";   
-        }
-    }
+        };
+}
+    
+function LovesFilter(){
+    return function(input){
+        input=input || "";
+        input = input.replace("likes","loves");
+        return input;
+    };
+}
+    
+function TruthFilter(){
+    return function(input,target,replace){
+        input=input || "";
+        input = input.replace(target,replace);
+        return input;
+    };
+}
+    
+    
 
 })();
